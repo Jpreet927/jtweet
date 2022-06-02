56a5b58191d84a467f1d606e38993d52cdb6e0e5
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-06-01 20:27:34 -0400

    fetched profiles that user is not following for suggested widget

--- src/Components/RightSidebar/Suggested.jsx
+++ src/Components/RightSidebar/Suggested.jsx
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+    collection,
+    onSnapshot,
+    query,
+    where,
+    limit,
+} from "firebase/firestore";
+import { db } from "../../Firebase/firebase";
+import { useUserAuth } from "../../Context/UserAuthContext";
 import { useThemeContext } from "../../Context/ThemeContext";
 import SuggestedProfile from "./SuggestedProfile";
 import "../../Styles/Suggested/Suggested.css";
 
 function Suggested() {
     const { theme } = useThemeContext();
+    const { userDoc } = useUserAuth();
+    const [suggestedUsers, setSuggestedUsers] = useState([]);
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(
+                    collection(db, "users"),
+                    where(
+                        "uid",
+                        "not-in",
+                        userDoc.following.concat(userDoc.uid)
+                    ),
+                    limit(5)
+                ),
+                (snapshot) => {
+                    setSuggestedUsers(snapshot.docs.map((doc) => doc.data()));
+                    console.log(snapshot.docs.map((doc) => doc.data()));
+                }
+            ),
+        []
+    );
 
     return (
         <div className={`${theme} suggested__container`}>
             <h3>Suggested Profiles</h3>
             <div className="suggested__profiles">
-                <SuggestedProfile isFollowing={true} />
-                <SuggestedProfile isFollowing={false} />
-                <SuggestedProfile isFollowing={true} />
-                <SuggestedProfile isFollowing={false} />
-                <SuggestedProfile isFollowing={true} />
+                {suggestedUsers.map((user) => (
+                    <SuggestedProfile
+                        key={user.uid}
+                        user={user}
+                        isFollowing={false}
+                    />
+                ))}
             </div>
         </div>
     );
--- src/Components/RightSidebar/SuggestedProfile.jsx
+++ src/Components/RightSidebar/SuggestedProfile.jsx
@@ -1,55 +1,91 @@
 import React, { useState, useEffect } from "react";
-import Avatar from "../Misc/Avatar";
+import { Link } from "react-router-dom";
+import {
+    collection,
+    doc,
+    onSnapshot,
+    updateDoc,
+    arrayRemove,
+    arrayUnion,
+} from "firebase/firestore";
+import { db } from "../../Firebase/firebase";
+import { useUserAuth } from "../../Context/UserAuthContext";
 import "../../Styles/SuggestedProfile/SuggestedProfile.css";
 
 function SuggestedProfile(props) {
-  const { isFollowing } = props;
-  const [following, setFollowing] = useState(isFollowing);
-  const [buttonText, setButtonText] = useState("Following");
+    const { user, isFollowing } = props;
+    const { userDoc } = useUserAuth();
+    const [following, setFollowing] = useState(isFollowing);
+    const [buttonText, setButtonText] = useState("Following");
+    const profileUserDocRef = doc(db, "users", user.uid);
+    const loggedUserDocRef = doc(db, "users", userDoc.uid);
 
-  const handleMouseOver = () => {
-    if (following) {
-      setButtonText("Unfollow");
-    }
-  };
-
-  const handleMouseLeave = () => {
-    if (following) {
-      setButtonText("Following");
-    }
-  };
-
-  const handleButtonClick = () => {
-    setFollowing(!following);
-  };
-
-  // useEffect(() => {
-  //   console.log(following);
-  // }, []);
-
-  return (
-    <div className="suggestedprofile__container">
-      <div className="suggestedprofile__details">
-        <Avatar dimension={"50px"} />
-        <div className="suggestedprofile__details-name">
-          <h3>Jaipreet Singh</h3>
-          <p>@jpreet</p>
-        </div>
-      </div>
-      <button
-        className={
-          following === true
-            ? "suggestedprofile__following"
-            : "suggestedprofile__follow"
+    const handleFollow = async () => {
+        // write to firebase - add logged in user to profiles followers, add profile to logged in users following
+        if (isFollowing === true) {
+            // unfollow
+            try {
+                await updateDoc(profileUserDocRef, {
+                    followers: arrayRemove(userDoc.uid),
+                });
+                await updateDoc(loggedUserDocRef, {
+                    following: arrayRemove(user.uid),
+                });
+            } catch (err) {}
+        } else {
+            // follow
+            try {
+                await updateDoc(profileUserDocRef, {
+                    followers: arrayUnion(userDoc.uid),
+                });
+                await updateDoc(loggedUserDocRef, {
+                    following: arrayUnion(user.uid),
+                });
+            } catch (err) {}
         }
-        onMouseOver={handleMouseOver}
-        onMouseLeave={handleMouseLeave}
-        onClick={handleButtonClick}
-      >
-        {following === true ? buttonText : "Follow"}
-      </button>
-    </div>
-  );
+        isFollowing ? setFollowing("Follow") : setFollowing("Following");
+        setFollowing(!following);
+    };
+
+    const handleMouseOver = () => {
+        if (following) {
+            setButtonText("Unfollow");
+        }
+    };
+
+    const handleMouseLeave = () => {
+        if (following) {
+            setButtonText("Following");
+        }
+    };
+
+    return (
+        <div className="suggestedprofile__container">
+            <div className="suggestedprofile__details">
+                <Link to={`/${user.uid}`} style={{ textDecoration: "none" }}>
+                    <div className="suggestedprofile__avatar">
+                        <img src={user.avatar} alt="" />
+                    </div>
+                </Link>
+                <div className="suggestedprofile__details-name">
+                    <h3>{user.name}</h3>
+                    <p>@{user.username}</p>
+                </div>
+            </div>
+            <button
+                className={
+                    following === true
+                        ? "suggestedprofile__following"
+                        : "suggestedprofile__follow"
+                }
+                onMouseOver={handleMouseOver}
+                onMouseLeave={handleMouseLeave}
+                onClick={handleFollow}
+            >
+                {following === true ? buttonText : "Follow"}
+            </button>
+        </div>
+    );
 }
 
 export default SuggestedProfile;
